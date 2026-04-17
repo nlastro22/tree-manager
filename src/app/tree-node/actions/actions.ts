f999011693638c6568ff369980dd385bb60e8aa7
nlastro22 <nlastro22@student.foi.hr>
2026-04-17 13:25:27 +0200

    actions: zamijeniti gumbe s izbornikom s operacijama za rad nad cvorom

--- src/app/tree-node/actions/actions.ts
+++ src/app/tree-node/actions/actions.ts
@@ -1,10 +1,11 @@
 import { Component, output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-actions',
-  imports: [MatIconModule, MatButtonModule],
+  imports: [MatIconModule, MatButtonModule, MatMenuModule],
   templateUrl: './actions.html',
   styleUrl: './actions.scss',
 })
